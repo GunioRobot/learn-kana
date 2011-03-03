4d5c11cd3dfbe18665734537b0d7081ec0df3df0
Wilhelm Joys Andersen <wilhelm@e-tjenesten.org>
2011-03-03 01:17:24 +0100

    Generate word list based on the chosen levels

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function generate_word_list(level, available_words) {
+  var words = new Array;
+
+  for (var i = 0; i < available_words.length; i++ ) {
+    if (level[i] == true) {
+      words = words.concat(available_words[i]);
+    }
+  }
+  return words;
+}
+
+function set_new_word(words) {
+  var word = random_word(words);
+  document.getElementById('kana').innerHTML = word;
+  return word;
+}
+
 function random_word(words) {
   return words[Math.floor(Math.random()*words.length)];
 }
@@ -9,12 +26,6 @@ function transliterate(word) {
     output = output + hiragana[word.charAt(i)];
   }
   return output;
-}
-
-function set_new_word() {
-  word = random_word(words);
-  document.getElementById('kana').innerHTML = word;
-  return word;
 }
 
 function show_correct_message(result) {
@@ -29,7 +40,11 @@ function show_incorrect_message(result, kana, answer) {
 
 window.onload = function(){
 
-  var kana = set_new_word();
+  var level = [true, true, true, true, true];
+
+  var words = [];
+  words = generate_word_list(level, available_words);
+  var kana = set_new_word(words);
   var romaji = transliterate(kana);
 
   var input = document.getElementById('input');
@@ -48,12 +63,13 @@ window.onload = function(){
       } else {
         show_incorrect_message(result, kana, romaji);
       }
-      kana = set_new_word();
+      kana = set_new_word(words);
       romaji = transliterate(kana);
       input.value = '';
       input.focus();
     }
   }
+
 }
 
 
